8c8092657cfe97905c9c612352dc310a0ab11f25
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-02-17 13:40:33 +0300

    Update firebase.js

--- mini-journal-app/src/firebase.js
+++ mini-journal-app/src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBOrG2IavkDkAFIrA_u6ixEdgA59onwhA",
-    authDomain: "notebook-app-7a25b.firebaseapp.com",
-    projectId: "notebook-app-7a25b",
-    storageBucket: "notebook-app-7a25b.firebasestorage.app",
-    messagingSenderId: "83097244893",
-    appId: "1:83097244893:web:e8ad7099430cd65b6d7734"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
